f1b2de80399a2b6099ab0bdf3e8d027ddb901804
lucasdanelon <lucascaldasdanelon@gmail.com>
2020-07-30 17:53:36 -0300

    Tratamento da base e criacao do score dos autores

--- algortimo_dt.py
+++ algortimo_dt.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt 
 
 #Ler a base de dados
 base = pd.read_csv("scopus.csv", sep = ",")
@@ -10,7 +11,7 @@ columns_to_drop = ["Author(s) ID","Volume","Issue",
                    "Page count","Link","Authors with affiliations",
                    "Abstract","Author Keywords", "Index Keywords", "References",
                    "Conference location","Conference code", "Abbreviated Source Title",
-                   "Document Type", "Publication Stage", "Access Type", "Source", "EID", 
+                   "Document Type", "Publication Stage", "Access Type", "Source", "EID",
                    "Sponsors", "Conference name", "Conference date"]
 
 base = base.drop(columns = columns_to_drop)
@@ -18,21 +19,57 @@ base = base.drop(columns = columns_to_drop)
 #Selecionar apenas artigos com DOI
 base = base[base.DOI.notnull()]
 
+# Select the papers in which the author name is available
+base = base[base.Authors != "[No author name available]"]
+
 #Mostrar a base de dados final
 print(base)
 
-#Criar uma base com 2 colunas: nome do autor (sem duplicatas) / score / (papers do autor)
-autores = ["author", "score", np.ones((1,2))]
+#Criar um DataFrame base com 2 colunas: nome do autor (sem duplicatas) / score / (papers do autor)
+autores = {}
 
-print(len(autores))
+print(autores)
+
+def score(n_authors, p_author):
+    coef = 1.5**(n_authors - p_author)/(np.sum(1.5**(np.arange(1,n_authors+1)-1)))
+    return coef
+
+for paper in range(base.shape[0]):
+    author_list = base.iloc[paper].Authors.split(",")
+    author_list = [authors.strip() for authors in author_list]
+
+    author_list = [author for author in author_list if '(' not in author]
+    author_list = [author for author in author_list if '[' not in author]
+    #remove (1), (2), (3)
+    # index = [idx for idx, string in enumerate(author_list) if '(' in string]
+    # for i in index:
+    #     del author_list[i]
+
+    n_authors = len(author_list)
+    for pos,author in enumerate(author_list):
+        if author in autores:
+            autores[author] += score(n_authors,pos)
+        else:
+            autores[author] = score(n_authors,pos)
+
+result = {}
+result["Authors"] = []
+result["Scores"] = []
+
+for key,value in autores.items():
+
+    result["Authors"] += [key]
+    result["Scores"] += [value]
+
+final = pd.DataFrame(data = result)
 
 """
 #Contar numero de autores de cada paper
-n_authors = sum(line.Authors(",")) for line in base
+n_authors = [line.Authors.split(",") for line in base]
 
 print(n_authors)
 
-#score = (1.5**(n_authors - p_author)/
+score = (1.5**(n_authors - p_author)/
 
 
 
